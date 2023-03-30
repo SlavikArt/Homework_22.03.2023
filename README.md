34deb7b66dd74ff441df26d56037f2c222255453
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-03-30 12:07:05 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Homework_22.03.2023
+Д/з: Employers, Equations
+
+<a href="https://gist.github.com/SlavikArt/cd35bd3846baedd3e2340e9d70212ca5">Gist - страница со всеми кодами проектов</a>
+
+* [main](main)
+
+<p align="center">
+    <h2>Main</h2>
+    <p></p>
+    <img src="images/main.png">
+</p>
---
+++ main/main.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+using namespace std;
+
+class Employer
+{
+public:
+    virtual void Print() = 0;
+};
+
+class President : public Employer
+{
+public:
+    virtual void Print()
+    {
+        cout << "This is a president.\n";
+    }
+};
+
+class Manager : public Employer
+{
+public:
+    virtual void Print()
+    {
+        cout << "This is a manager.\n";
+    }
+};
+
+class Worker : public Employer
+{
+public:
+    virtual void Print()
+    {
+        cout << "This is a worker.\n";
+    }
+};
+
+class Equation
+{
+public:
+    virtual void roots() = 0;
+};
+
+class LinearEquation : public Equation // ax = b
+{
+    double a, b;
+public:
+    LinearEquation(double A, double B) : a(A), b(B)
+    {
+    }
+
+    void roots()
+    {
+        if (a == 0 && b == 0)
+        {
+            cout << "Уравнение имеет бесконечно много корней." << "\n";
+        }
+        else if (a == 0 && b != 0)
+        {
+            cout << "Уравнение не имеет корней." << "\n";
+        }
+        else
+        {
+            double x = -b / a;
+            cout << "Уравнение имеет один корень: " << x << "\n";
+        }
+    }
+};
+
+class QuadraticEquation : public Equation // ax^2 + bx + c = 0
+{
+    double a, b, c;
+public:
+    QuadraticEquation(double A, double B, double C) : a(A), b(B), c(C)
+    {
+    }
+
+    void roots()
+    {
+        double D = b * b - 4 * a * c;
+
+        if (a == 0)
+        {
+            LinearEquation eq(b, c);
+            eq.roots();
+        }
+        else if (D > 0)
+        {
+            double x1 = (-b + sqrt(D)) / (2 * a);
+            double x2 = (-b - sqrt(D)) / (2 * a);
+            cout << "Уравнение имеет два корня: " << x1 << " и " << x2 << "\n";
+        }
+        else if (D == 0)
+        {
+            double x = -b / (2 * a);
+            cout << "Уравнение имеет один корень: " << x << "\n";
+        }
+        else
+        {
+            cout << "Уравнение не имеет действительных корней.\n";
+        }
+    }
+};
+
+int main()
+{
+    setlocale(0, "ukr");
+
+    Employer* employers[3] = { new President(), new Manager(), new Worker() };
+
+    for (int i = 0; i < 3; i++)
+        employers[i]->Print();
+
+    LinearEquation eq1(0, 5); // ax = b, 0x = 5
+    eq1.roots();
+
+    QuadraticEquation eq2(5, 3, -26); // ax^2 + bx + c = 0, 5x^2 + 3x - 26 = 0
+    eq2.roots();
+}
